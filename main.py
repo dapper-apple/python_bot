ed3393d4266f16b853bf9d6ed0ae8b6b8588ed8a
dapper-apple <55886224+dapper-apple@users.noreply.github.com>
2020-06-09 23:10:11 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+from telegram.ext.dispatcher import run_async
+import requests
+import re
+
+def get_url():
+    contents = requests.get('https://random.cat/meow.json').json()
+    url = contents['url']
+    return url
+
+def get_image_url():
+    allowed_extension = ['jpg','jpeg','png']
+    file_extension = ''
+    while file_extension not in allowed_extension:
+        url = get_url()
+        file_extension = re.search("([^.]*)$",url).group(1).lower()
+    return url
+
+@run_async
+def chonk(bot, update):
+    url = get_image_url()
+    chat_id = update.message.chat_id
+    context.bot.send_photo(chat_id=chat_id, photo=url)
+
+def main():
+    updater = Updater('YOUR_TOKEN')
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler('chonk',chonk))
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
